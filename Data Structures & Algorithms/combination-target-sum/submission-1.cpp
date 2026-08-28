5fec6ba28614dd400097a1176a5c2d09c298df4d
Tushar <76674820+Tushar07082@users.noreply.github.com>
2026-08-28 13:13:24 +0530

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void findCombinations(vector<int> &nums, int i, int target, vector<vector<int>> &ans, vector<int> &temp){
+        if(target == 0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        if(i == nums.size()) {
+            return;
+        }
+        
+        findCombinations(nums, i+1, target, ans, temp);
+
+        if(nums[i] <= target){
+            temp.push_back(nums[i]);
+            findCombinations(nums, i, target-nums[i], ans, temp);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+       vector<vector<int>> ans;
+       vector<int> temp;
+       findCombinations(nums, 0, target, ans, temp);
+       return ans; 
+    }
+};
