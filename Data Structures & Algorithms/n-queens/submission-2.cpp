76d79de45fbfb9965a6d355ae49f67e56936a7a6
Tushar <76674820+Tushar07082@users.noreply.github.com>
2026-09-06 12:14:15 +0530

    Add: n-queens - submission-2

---
+++ Data Structures & Algorithms/n-queens/submission-2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    unordered_set<int> colsDone;
+    unordered_set<int> backDiagDone;
+    unordered_set<int> frontDiagDone;
+    void findSols(vector<vector<string>> &ans, vector<string> &curr, int i){
+        if(i==curr.size()){
+            ans.push_back(curr);
+            return;
+        }
+
+        for(int j=0;j<curr.size();j++){
+            // cout<<i<<" "<<j<<" "<<colsDone[j]<<" "<<backDiagDone[i-j]<<" "<<frontDiagDone[i+j]<<endl;
+            if(colsDone.count(j) == 0 && backDiagDone.count(i-j) == 0 && frontDiagDone.count(i+j) == 0){
+                // cout<<i<<" "<<j<<endl;
+                curr[i][j] = 'Q';
+                colsDone.insert(j);
+                backDiagDone.insert(i-j);
+                frontDiagDone.insert(i+j);
+                findSols(ans, curr, i+1);
+                curr[i][j] = '.';
+                colsDone.erase(j);
+                backDiagDone.erase(i-j);
+                frontDiagDone.erase(i+j);
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> curr(n, string(n, '.'));
+        findSols(ans, curr, 0);
+        return ans;
+    }
+};
