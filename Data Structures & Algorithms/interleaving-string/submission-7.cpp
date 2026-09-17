457cccf27177218345c8a707a3c17a88175ff62b
Tushar <76674820+Tushar07082@users.noreply.github.com>
2026-09-18 00:10:13 +0530

    Add: interleaving-string - submission-7

---
+++ Data Structures & Algorithms/interleaving-string/submission-7.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    bool solve(string &s1, string &s2, string &s3, int i, int j){
+        if(i==0 && j==0) return true;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int k = i + j;
+        bool ans = false;
+
+        if(i>0 && s1[i-1]==s3[k-1]){
+            ans = ans || solve(s1, s2, s3, i-1, j);
+        }
+        if(ans) return dp[i][j] = ans;
+
+        if(j>0 && s2[j-1]==s3[k-1]){
+            ans = ans || solve(s1, s2, s3, i, j-1);
+        }
+        return dp[i][j] = ans;
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        int x = s1.size(), y = s2.size();
+        if (x + y != s3.size()) {
+            return false;
+        }
+        if(x==0) return s2==s3;
+        if(y==0) return s1==s3;
+        dp.assign(x+1, vector<int> (y+1, -1));
+        return solve(s1, s2, s3, x, y);
+    }
+};
