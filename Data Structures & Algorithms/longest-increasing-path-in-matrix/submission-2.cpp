8d84d5ecc884978a969412f4c8d5f139aa32f369
Tushar <76674820+Tushar07082@users.noreply.github.com>
2026-09-24 20:08:44 +0530

    Add: longest-increasing-path-in-matrix - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-2.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int solve(vector<vector<int>> &matrix, int i, int j, int last){
+        if(i<0 || j<0 || i>=matrix.size() || j>= matrix[0].size() || matrix[i][j] <= last){
+            return 0;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = 1 + max({
+            solve(matrix, i+1, j, matrix[i][j]),
+            solve(matrix, i-1, j, matrix[i][j]),
+            solve(matrix, i, j+1, matrix[i][j]),
+            solve(matrix, i, j-1, matrix[i][j])
+        });
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int ans = 1, n = matrix.size(), m= matrix[0].size();
+        dp.resize(n, vector<int> (m, -1));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                ans = max(ans, solve(matrix, i, j, -1));
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        // [7,7,5]
+        // [2,4,6]
+        // [8,2,0]
+
+        // 1 1 2 
+        // 3 2 1 
+        // 1 3 -1 
+        return ans;
+    }
+};
