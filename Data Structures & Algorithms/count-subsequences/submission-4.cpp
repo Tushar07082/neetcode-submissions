ec548489c939e33a081a01e801b8523058aa7380
Tushar <76674820+Tushar07082@users.noreply.github.com>
2026-09-24 20:24:15 +0530

    Add: count-subsequences - submission-4

---
+++ Data Structures & Algorithms/count-subsequences/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int numDistinct(string s, string t) {
+        int n = s.size(), m = t.size();
+        vector<int> dp(m+1, 0);
+
+        dp[m] = 1;
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<m;j++){
+                if(s[i]==t[j]){
+                    dp[j] += dp[j+1];
+                }
+            }
+        }
+        return dp[0];
+    }
+};
